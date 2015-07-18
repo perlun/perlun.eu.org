b3da16b0e59beba6e1750ac0ebe794aa645d5f98
Per Lundberg <perlun@gmail.com>
2015-07-19 00:17:30 +0300

    Hacked the slugification so that it calls our ascii_sanitize first.

--- _plugins/permalink_rewriter.rb
+++ _plugins/permalink_rewriter.rb
@@ -1,19 +1,9 @@
 module Jekyll
-  # Inspired by http://stackoverflow.com/a/17206081/227779
-  class PermalinkRewriter < Generator
-    safe true
-    priority :low
+  module Utils
+    alias_method :slugify_real, :slugify
 
-    def generate(site)
-      site.posts.each do |item|
-        item.data['permalink'] = '/' + [
-          ascii_sanitize(item.categories.join('/')),
-          item.date.year,
-          item.date.month,
-          item.date.day,
-          item.slug
-        ].join('/')
-      end
+    def slugify(string)
+      slugify_real(ascii_sanitize(string))
     end
 
     def ascii_sanitize(s)
@@ -23,6 +13,24 @@ module Jekyll
       result.gsub! /ö/, 'o'
       result.gsub! ' ', '-'
       result
+    end
+  end
+
+  # Inspired by http://stackoverflow.com/a/17206081/227779
+  class PermalinkRewriter < Generator
+    safe true
+    priority :low
+
+    def generate(site)
+      site.posts.each do |item|
+        item.data['permalink'] = '/' + [
+          Utils.ascii_sanitize(item.categories.join('/')),
+          item.date.year,
+          item.date.month,
+          item.date.day,
+          item.slug
+        ].join('/')
+      end
     end
   end
 end
