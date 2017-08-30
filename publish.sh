17a6d3fa8f35d99765380d37898fe8e380d2b817
Per Lundberg <per.lundberg@ecraft.com>
2017-08-30 12:20:27 +0300

    Fixed publish script.

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll build && rsync --delete -gloprtv _site/ pi2:/var/www/perlun.eu.org
+bundle exec jekyll build && rsync --delete -gloprtv _site/ perlun.eu.org:/var/www/perlun.eu.org
