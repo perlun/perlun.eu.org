134a7ad6ac89d2b7e0c4ee8f2b3623b3e98b0ba4
Per Lundberg <perlun@gmail.com>
2018-10-20 23:24:10 +0300

    CI: Add auto-deploy
    Untested, will see now if it works :)

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+IP=85.134.56.45
+
+if [ -z `ssh-keygen -F $IP` ]; then
+  ssh-keyscan -H $IP >> ~/.ssh/known_hosts
+fi
+
+rsync -e 'ssh -oBatchMode=yes' -gloprtv --delete ./_site/ www-data@$IP:perlun.eu.org
