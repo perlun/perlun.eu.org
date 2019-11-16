cde8b39851f7c87303aad9afa6dca84d33915c33
Per Lundberg <perlun@gmail.com>
2019-11-16 22:09:38 +0200

    deploy.sh: add touch command to forcibly set file dates
    This is an attempt to avoid syncing all files every time.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,8 @@ if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
 
+# Reset all file & directory timestamps to beginning of epoch, to make
+# rsync avoid treating all files as modified.
+find _site -exec touch -t 197001010000 {} +
+
 rsync -e 'ssh -oBatchMode=yes' -gloprtv --delete ./_site/ www-data@$IP:perlun.eu.org
